2e6452f0f8ca205b90d61714e0c454681ff780e4
Mateusz Podlasin <mateusz@stoplight.io>
2020-12-02 10:48:05 +0100

    feat: add basic e2e checks to api and stoplight components

--- cypress/integration/api.js
+++ cypress/integration/api.js
@@ -1,9 +1,20 @@
 describe('API component', () => {
-   beforeEach(() => {
-       cy.visit("http://localhost:4200/");
-   });
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/zoom-api');
+  });
 
-   it('Loads correctly', () => {
-       cy.get("h1:contains('Introduction')").should('exist');
-   })
+  it('loads correctly', () => {
+    cy.get('h1').contains('Zoom').should('exist');
+  });
+
+  it('navigates via table of contents', () => {  
+    cy.get('span').contains('Groups').click();
+    cy.get('span').contains('Create a group').click();
+    cy.get('p').contains('Create a group under your account').should('exist');
+  });
+  
+  it('navigates via url', () => {
+    cy.visit('http://localhost:4200/zoom-api/paths/~1groups/post'); 
+    cy.get('p').contains('Create a group under your account').should('exist');
+  });
 });
