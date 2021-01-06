e1a9973a9d1aa09040e7633b5837ee8310616e1f
Marcell Toth <marcell@marcelltoth.net>
2021-01-06 15:01:53 +0100

    test: fix flake

--- cypress/integration/stoplight.js
+++ cypress/integration/stoplight.js
@@ -1,22 +1,26 @@
-describe('Stoplight component', () => {
+describe("Stoplight component", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:4200/');
+    cy.visit("http://localhost:4200/");
+    // wait until we perform the full redirect to the first page to avoid flake
+    cy.get("h1").contains("Introduction").should("exist");
   });
 
-  it('loads correctly', () => {
-    cy.get('h1').contains('Introduction').should('exist');
+  it("loads correctly", () => {
+    cy.get("h1").contains("Introduction").should("exist");
   });
 
-  it('navigates via table of contents', () => {  
-    cy.get('span').contains('pets').click();
-    cy.get('span').contains('Create a pet').click();
-    cy.get('h2').contains('post').should('exist');
-    cy.get('h2').contains('/pets').should('exist');
+  it("navigates via table of contents", () => {
+    cy.get("span").contains("pets").click();
+    cy.get("span").contains("Create a pet").click();
+    cy.get("h2").contains("post").should("exist");
+    cy.get("h2").contains("/pets").should("exist");
   });
-  
-  it('navigates via url', () => {
-    cy.visit('http://localhost:4200/elements/reference/petstore/openapi.v1.yaml/paths/~1pets/post'); 
-    cy.get('h2').contains('post').should('exist');
-    cy.get('h2').contains('/pets').should('exist');
+
+  it("navigates via url", () => {
+    cy.visit(
+      "http://localhost:4200/elements/reference/petstore/openapi.v1.yaml/paths/~1pets/post"
+    );
+    cy.get("h2").contains("post").should("exist");
+    cy.get("h2").contains("/pets").should("exist");
   });
 });
