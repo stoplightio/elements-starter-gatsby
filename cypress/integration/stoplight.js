c5ad83413b6b619ad72bd926c207f69e07ae0ee3
mmiask <mmiask92@gmail.com>
2021-01-12 18:02:18 +0100

    test: fix flake
    remove duplicated cy.visit
    specify selector for cy.contains

--- cypress/integration/stoplight.js
+++ cypress/integration/stoplight.js
@@ -1,26 +1,24 @@
 describe("Stoplight component", () => {
   beforeEach(() => {
     cy.visit("http://localhost:4200/");
-    // wait until we perform the full redirect to the first page to avoid flake
-    cy.get("h1").contains("Introduction").should("exist");
   });
 
   it("loads correctly", () => {
-    cy.get("h1").contains("Introduction").should("exist");
+    cy.contains("h1", "Introduction").should("exist");
   });
 
   it("navigates via table of contents", () => {
-    cy.get("span").contains("pets").click();
-    cy.get("span").contains("Create a pet").click();
-    cy.get("h2").contains("post").should("exist");
-    cy.get("h2").contains("/pets").should("exist");
+    cy.contains("span", "pets").click();
+    cy.contains("span", "Create a pet").click();
+    cy.contains("h2", "post").should("exist");
+    cy.contains("h2", "/pets").should("exist");
   });
 
   it("navigates via url", () => {
     cy.visit(
       "http://localhost:4200/elements/reference/petstore/openapi.v1.yaml/paths/~1pets/post"
     );
-    cy.get("h2").contains("post").should("exist");
-    cy.get("h2").contains("/pets").should("exist");
+    cy.contains("h2", "post").should("exist");
+    cy.contains("h2", "/pets").should("exist");
   });
 });
